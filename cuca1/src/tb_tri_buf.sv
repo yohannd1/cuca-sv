7ac25b13891e767f7025d6ceb6998a24db18e390
yohannd1 <mitonanan12@gmail.com>
2025-07-28 21:39:57 -0300

    tri_buf: change bus type to wire, add test for actual multiple driving

--- cuca1/src/tb_tri_buf.sv
+++ cuca1/src/tb_tri_buf.sv
@@ -2,33 +2,72 @@
 
 module tb_tri_buf;
   localparam WIDTH = 8;
-  typedef logic[WIDTH-1:0] bus_t;
+  typedef logic[WIDTH-1:0] data_t;
 
-  logic rw;
-  bus_t bus, data;
-  tri_buf #(.WIDTH(8)) uut(.*);
+  module test_single;
+    logic rw;
+    wire[WIDTH-1:0] bus;
+    logic[WIDTH-1:0] data;
+    tri_buf #(.WIDTH(WIDTH)) uut(.*);
 
-  localparam bus_t all_z = 'bZ;
+    task test();
+      rw = 0;
+      #1;
+      assert (bus === 'z);
+
+      data = 5;
+      rw = 1;
+      #1;
+      assert (bus == 5);
+
+      rw = 0;
+      data = 6;
+      #1;
+      assert (bus === 'z);
+
+      rw = 1;
+      #1;
+      assert (bus == 6);
+    endtask
+  endmodule
+
+  module test_multiple;
+    wire[WIDTH-1:0] bus;
+    logic[WIDTH-1:0] data_0, data_1;
+    logic rw_0, rw_1;
+
+    tri_buf #(.WIDTH(WIDTH)) u0(.bus(bus), .rw(rw_0), .data(data_0));
+    tri_buf #(.WIDTH(WIDTH)) u1(.bus(bus), .rw(rw_1), .data(data_1));
+
+    task test();
+      data_0 = 15;
+      data_1 = 20;
+
+      rw_0 = 0;
+      rw_1 = 0;
+      #1;
+      assert (bus === 'z);
+
+      rw_0 = 1;
+      rw_1 = 0;
+      #1;
+      assert (bus === 15);
+
+      rw_0 = 0;
+      rw_1 = 1;
+      #1;
+      assert (bus === 20);
+
+      rw_0 = 1;
+      rw_1 = 1;
+      #1;
+      assert ($countbits(bus, 'x) > 0);
+    endtask
+  endmodule
 
   initial begin
-    rw = 0;
-    #1;
-    assert (bus === all_z);
-
-    data = 5;
-    rw = 1;
-    #1;
-    assert (bus == 5);
-
-    rw = 0;
-    data = 6;
-    #1;
-    assert (bus === all_z);
-
-    rw = 1;
-    #1;
-    assert (bus == 6);
-
+    test_single.test();
+    test_multiple.test();
     $finish;
   end
 endmodule
