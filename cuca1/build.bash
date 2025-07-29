801fad024993eff89c6f072c31b3378081e4470a
yohannd1 <mitonanan12@gmail.com>
2025-07-28 21:39:45 -0300

    cuca1: improve build script

--- cuca1/build.bash
+++ cuca1/build.bash
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
-set -e
+set -ue
+progname=$(basename "$0")
+
+showHelp() {
+  cat >&2 <<EOF
+Usage: $progname <TB_NAME>
+EOF
+  exit 2
+}
 
 run() {
   printf >&2 "%s\n" "$*"
   "$@"
 }
 
+[ $# != 1 ] && showHelp || true
+
+declare -A tbs
+tbs[ram]="src/tb_ram.sv"
+tbs[tri_buf]="src/tb_tri_buf.sv"
+
 sources=(src/tri_buf.sv src/ram.sv)
-testbench=src/tb_tri_buf.sv
+testbench=${tbs[$1]}
 
 run mkdir -p build
 run iverilog -g2012 "${sources[@]}" "$testbench" -o build/out.com
