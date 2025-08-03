c486af1194b5f25b321aee69ad113a10900f595f
yohannd1 <mitonanan12@gmail.com>
2025-08-02 21:42:26 -0300

    preliminary synth script with yosys

--- cuca1/build.bash
+++ cuca1/build.bash
@@ -5,7 +5,7 @@ progname=$(basename "$0")
 
 showHelp() {
   cat >&2 <<EOF
-Usage: $progname <TB_NAME>
+Usage: $progname { test <NAME> | synth }
 EOF
   exit 2
 }
@@ -15,17 +15,36 @@ run() {
   "$@"
 }
 
-[ $# != 1 ] && showHelp || true
+[ $# -gt 0 ] || showHelp
 
 declare -A tbs
 tbs[ram]="src/tb_ram.sv"
 tbs[tri_buf]="src/tb_tri_buf.sv"
 tbs[register]="src/tb_register.sv"
 tbs[alu]="src/tb_alu.sv"
+tbs[cuca1]="src/tb_cuca1.sv"
 
-sources=(src/tri_buf.sv src/ram.sv src/register.sv src/alu.sv)
-testbench=${tbs[$1]}
+sources=(src/tri_buf.sv src/ram.sv src/register.sv src/alu.sv src/cuca1.sv)
 
-run mkdir -p build
-run iverilog -g2012 "${sources[@]}" "$testbench" -o build/out.com
-run ./build/out.com
+if [ ! -d build ]; then
+  printf >&2 "build dir not found - creating it...\n"
+  mkdir build
+fi
+
+case "$1" in
+  test)
+    [ $# = 2 ] || showHelp
+    testbench=${tbs[$2]}
+    run iverilog -g2012 "${sources[@]}" "$testbench" -o build/out.com
+    run ./build/out.com
+    ;;
+  synth)
+    [ $# = 1 ] || showHelp
+    run sv2v -w build/all.v "${sources[@]}"
+    run yosys -p '
+      read_verilog build/all.v;
+      # show -format dot -prefix build/alu alu;
+    '
+    ;;
+  *) showHelp ;;
+esac
