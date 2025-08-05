84d25c0fe931bc750f59f88c308d0898c15a5ca4
yohannd1 <mitonanan12@gmail.com>
2025-08-05 18:15:20 -0300

    cuca1: more work on the processor...

--- cuca1/src/cuca1.sv
+++ cuca1/src/cuca1.sv
@@ -22,47 +22,41 @@ module cuca1(
     PIN_MAX
   } enum_microprogram_pin;
 
-  typedef enum {
-    STATE_FETCH,
-    STATE_DECODE,
-    STATE_EXECUTE,
-    STATE_MAX
-  } enum_state;
-
   typedef logic[$clog2(PIN_MAX)-1:0] mcprog_line_t;
-  typedef logic[$clog2(STATE_MAX)-1:0] state_t;
 
-  // Constants
   localparam mcprog_line_t nothing = 'b0;
+
+  localparam MCPROG_SIZE = 128;
+  typedef logic[$clog2(MCPROG_SIZE)-1:0] mcprog_ptr_t;
+  mcprog_line_t mcprog_mem[MCPROG_SIZE];
 
   wire[BITW-1:0] bus;
 
   alu_op_t alu_op;
   alu alu_(.clock(clock), .n_reset(n_reset), .op(alu_op), .bus(bus));
 
-  register acc(.clock(clock), .rd_en(current[PIN_ACC_RD]), .wr_en(current[PIN_ACC_WR]));
-  register pc(.clock(clock), .rd_en(current[PIN_PC_RD]), .wr_en(current[PIN_PC_WR]));
-  register ir(.clock(clock), .rd_en(current[PIN_IR_RD]), .wr_en(current[PIN_IR_WR]));
+  mcprog_ptr_t mcprog_pc;
+  mcprog_line_t mcprog_cur;
+  assign mcprog_cur = mcprog_mem[mcprog_pc];
 
-  state_t state;
-  mcprog_line_t current;
+  register acc(.clock(clock), .rd_en(mcprog_cur[PIN_ACC_RD]), .wr_en(mcprog_cur[PIN_ACC_WR]));
+  register pc(.clock(clock), .rd_en(mcprog_cur[PIN_PC_RD]), .wr_en(mcprog_cur[PIN_PC_WR]));
+  register ir(.clock(clock), .rd_en(mcprog_cur[PIN_IR_RD]), .wr_en(mcprog_cur[PIN_IR_WR]));
 
   always_ff @(posedge clock) begin
     if (~n_reset) begin
-      current <= nothing;
-      state <= STATE_FETCH;
+      mcprog_pc <= 0;
+
+      mcprog_mem[0] = nothing;
+      mcprog_mem[0][PIN_PC_RD] = 1'b1;
+      mcprog_mem[0][PIN_MEM_RD] = 1'b1;
+      mcprog_mem[1] = nothing;
+      mcprog_mem[1][PIN_END] = 1'b1;
     end else begin
-      case (state)
-        STATE_FETCH: begin
-          // TODO
-        end
-        STATE_DECODE: begin
-          // TODO
-        end
-        STATE_EXECUTE: begin
-          // TODO
-        end
-      endcase
+      if (mcprog_cur[PIN_END])
+        mcprog_pc <= 0;
+      else
+        mcprog_pc <= mcprog_pc + 1;
     end
   end
 
--- cuca1/src/tb_cuca1.sv
+++ cuca1/src/tb_cuca1.sv
@@ -1,8 +1,15 @@
+`timescale 1ns/1ns
+
 module tb_cuca1;
   logic clock;
   logic n_reset;
-  logic[BITW-1:0] bus;
+  wire[BITW-1:0] bus;
 
   cuca1 uut(.clock(clock), .n_reset(n_reset), .ext_bus(bus));
-  ram memory(.clock(clock), .bus(ext_bus));
+  ram memory(.clock(clock), .bus(bus));
+
+  initial begin
+    #5;
+    $finish;
+  end
 endmodule
